4b36ace500f4725d6d13f7fbce2a549a6d5c5458
akrauss419 <akrauss419@gmail.com>
2022-10-07 14:52:47 -0700

    initial HTML & CSS + state variables

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/* -------- constants -------- */
+
+
+
+
+/*-------- state variables --------*/
+let minefield;
+let mines;
+let numberHints;
+let win;
+let loss;
+
+
+/*-------- cached elements --------*/
+
+
+
+
+/*-------- event listeners --------*/
+
+
+
+
+/*-------- functions --------*/
